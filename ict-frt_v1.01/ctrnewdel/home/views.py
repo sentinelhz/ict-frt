20ecd6c6376c73fc2623e3d06b019bbd7c8d9c1e
Kunaldz <kunal@xynocast.com>
2021-10-18 23:54:46 +0530

    commit new

--- ict-frt_v1.01/ctrnewdel/home/views.py
+++ ict-frt_v1.01/ctrnewdel/home/views.py
@@ -9,38 +9,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
-login_required(login_url="/login/")
+#@login_required(login_url="/login/")
+#def pages(request):
+ #   context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+  #  try:
+   #     load_template = request.path.split('/')[-1]
+
+#@login_required(login_url="/login/")
 def pages(request):
     context = {}
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        load_template = request.path.split('/')[-1]
-
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
 
         load_template = request.path.split('/')[-1]
 
-        if load_template == 'admin':
-            return HttpResponseRedirect(reverse('admin:index'))
-        context['segment'] = load_template
-
-        html_template = loader.get_template('home/' + load_template)
-        return HttpResponse(html_template.render(context, request))
-
-    except template.TemplateDoesNotExist:
-
-        html_template = loader.get_template('home/page-404.html')
-        return HttpResponse(html_template.render(context, request))
-
-    except:
-        html_template = loader.get_template('home/page-500.html')
-        return HttpResponse(html_template.render(context, request
         if load_template == 'admin':
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
@@ -60,7 +45,7 @@ def pages(request):
 
 #@login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
+    	context = {'segment': 'index'}
 
-    html_template = loader.get_template('home/index.html')
-    return HttpResponse(html_template.render(context, request)
+    	html_template = loader.get_template('home/index.html')
+    	return HttpResponse(html_template.render(context, request))
